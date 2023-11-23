8a1ca7bb883ff8187f372fe05bbc43e935abb52a
Juho Ahonen <juho.ahonen2@gmail.com>
2023-11-23 13:30:03 +0200

    testing

--- notes/src/App.jsx
+++ notes/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
 
@@ -9,31 +10,43 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
 
 
-  const hook = () => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
+  useEffect(() => {
+    noteService
+      .getAll()
       .then(response => {
-        console.log('promise fulfilled')
         setNotes(response.data)
       })
-  }
+  }, [])
+
+  
   
   useEffect(hook, [])
   console.log('render', notes.length, 'notes')
 
-  const addNote = (event) => {
+  const addNote = event => {
     event.preventDefault()
-    console.log('button clicked', event.target)
     const noteObject = {
       content: newNote,
-      date: new Date().toISOString(),
       important: Math.random() > 0.5,
-      id: notes.length + 1,
     }
+  
+    noteService
+      .create(noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
+  }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    noteService
+      .update(id, changedNote)
+      .then(response => {
+        setNotes(notes.map(note => note.id !== id ? note : response.data))
+      })
   }
 
   return (
@@ -44,7 +57,11 @@ const App = () => {
       </div>
       <ul>
         {notes.map(note =>
-          <Note key={note.id} note={note} />
+            <Note
+            key={note.id}
+            note={note} 
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
       <form onSubmit={addNote}>

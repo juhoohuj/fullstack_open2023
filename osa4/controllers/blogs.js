a286b03ae3a04cad33039f488f3721847c85ad6a
Juho Ahonen <juho.ahonen2@gmail.com>
2023-12-17 22:58:59 +0200

    4.19 valmis

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -2,6 +2,15 @@ const app = require('express')()
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 
 
@@ -16,12 +25,17 @@ blogsRouter.get('/', (request, response) => {
   blogsRouter.post('/', async (request, response) => {
     try {
       const { title, author, url, likes } = request.body;
+
+      const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+      if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+      }
   
       if (!title || !url) {
         return response.status(400).json({ error: 'Title and URL are required.' });
       }
   
-      const user = await User.findById("657d991c79b18ec1e1ab6ef0");
+      const user = await User.findById(decodedToken.id)
       console.log(user)
   
       const blog = new Blog({ title, author, url, likes, user: user._id });

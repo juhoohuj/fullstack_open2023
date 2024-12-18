060f9372d76eef6229a4c6d959383b5cad03a8f8
Juho Ahonen <juho.ahonen2@gmail.com>
2024-12-18 18:43:00 +0200

    8.16 valmis

--- osa8/library-backend.js
+++ osa8/library-backend.js
@@ -2,12 +2,19 @@ const { ApolloServer, gql } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
 const mongoose = require('mongoose');
 const { GraphQLError } = require('graphql');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 mongoose.set('strictQuery', false);
 const Author = require('./models/author');
 const Book = require('./models/book');
+const User = require('./models/user');
 require('dotenv').config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
+const JWT_SECRET = process.env.JWT_SECRET;
+
+console.log('JWT_SECRET:', JWT_SECRET);
+
 
 console.log('connecting to', MONGODB_URI);
 
@@ -20,12 +27,23 @@ mongoose
     console.log('error connection to MongoDB:', error.message);
   });
 
-  const typeDefs = `
+const typeDefs = `
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+    me: User
   }
 
   type Book {
@@ -54,6 +72,14 @@ mongoose
       name: String!
       setBornTo: Int!
     ): Author
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): Token
   }
 `;
 
@@ -82,9 +108,22 @@ const resolvers = {
         return { ...author._doc, bookCount };
       });
     },
+    me: (root, args, context) => {
+      return context.currentUser;
+    }
   },
   Mutation: {
-    addBook: async (root, args) => {
+    addBook: async (root, args, context) => {
+      console.log('Adding book:', args);
+      console.log('Current user:', context.currentUser);
+      if (!context.currentUser) {
+        throw new GraphQLError('Not authenticated', {
+          extensions: {
+            code: 'UNAUTHENTICATED',
+          },
+        });
+      }
+
       try {
         let author = await Author.findOne({ name: args.author });
         if (!author) {
@@ -104,7 +143,15 @@ const resolvers = {
         });
       }
     },
-    editAuthor: async (root, args) => {
+    editAuthor: async (root, args, context) => {
+      if (!context.currentUser) {
+        throw new GraphQLError('Not authenticated', {
+          extensions: {
+            code: 'UNAUTHENTICATED',
+          },
+        });
+      }
+
       try {
         const author = await Author.findOne({ name: args.name });
         if (!author) {
@@ -123,7 +170,58 @@ const resolvers = {
         });
       }
     },
-  },
+    createUser: async (root, args) => {
+      const existingUser = await User.findOne({ username: args.username });
+      if (existingUser) {
+        throw new GraphQLError('Username must be unique', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.username,
+          },
+        });
+      }
+
+      const saltRounds = 10;
+      const passwordHash = await bcrypt.hash('password', saltRounds);
+
+      const user = new User({ ...args, passwordHash });
+
+      try {
+        await user.save();
+      } catch (error) {
+        throw new GraphQLError(error.message, {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args,
+          },
+        });
+      }
+
+      return user;
+    },
+    login: async (root, args) => {
+      console.log('Logging in with username:', args.username);
+      const user = await User.findOne({ username: args.username });
+      const passwordCorrect = user === null
+        ? false
+        : await bcrypt.compare(args.password, user.passwordHash);
+
+      if (!(user && passwordCorrect)) {
+        throw new GraphQLError('Invalid username or password', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          },
+        });
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      };
+
+      return { value: jwt.sign(userForToken, JWT_SECRET) };
+    }
+  }
 };
 
 const server = new ApolloServer({
@@ -133,6 +231,36 @@ const server = new ApolloServer({
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
+  context: async ({ req }) => {
+    console.log('Context function invoked');
+    const auth = req.headers.authorization || null;
+    console.log('Authorization Header:', auth);
+
+    if (!auth || !auth.toLowerCase().startsWith('bearer ')) {
+      console.log('No valid authorization header.');
+      return {};
+    }
+
+    try {
+      const token = auth.substring(7);
+      console.log('Token:', token);
+
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+      console.log('Decoded Token:', decodedToken);
+
+      const currentUser = await User.findById(decodedToken.id);
+      console.log('Current User:', currentUser);
+
+      return { currentUser };
+    } catch (error) {
+      console.error('Error during token verification:', error.message);
+      return {};
+    }
+  },
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
